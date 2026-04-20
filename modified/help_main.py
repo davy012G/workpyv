8fd49f36f060114c6291bc75bc296e5a505e2940
davy_g <davethebro520@gmail.com>
2026-04-20 14:15:57 +0100

    renamed:    modified/Introduction -> modified/Introduction.txt 	new file:   modified/README.md 	new file:   modified/help_main.py 	new file:   modified/set_info.py

---
+++ modified/help_main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3 help_main.py
+
+#Filename: help_main.py
+
+class Main:
+
+    def __init__(self):
+        pass
+
+def main():
+    """The main function..."""
+    
+    print("\nHello World!\n")
+
+if __name__ == "__main__":
+    main()
---
+++ modified/set_info.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python3 set_info.py
+
+# Filename: set_info.py
+
+"""
+Documentation
+=============
+
+This is more of a convenient .json (JavaScript Object Notation) file than it is a .py (Python) file. So the name is a pun with
+the words 'settings' and 'info' which is just as simple to understand cuz it serves as the settings and the information storage
+for the entire codebase of Project::HelperBot.
+"""
+
+NAME : str = "HelperBot"
+VERSION : tuple[int, int, int] = (0, 1, 0)
+
+# Information
+WORK = """
+
+Work
+
+This is the work zone please state which tool you want to work with;
+
+1.Noteit - Helps you with texts and other issues you might want to put down in you computer.
+2.WorkBase - this includes your database and some data that you might want to save
+3.Advanced_Calc. - helps with your calculations and works like an artificial intelligence
+press any integer key to exit
+
+"""
+
+WORKBASE = """
+
+WorkBase
+
+Hello, Welcome to WorkBase here are statistical things you'ld like to work with
+1. Pie Chart - It only gives you values
+2. Bar Chart - Tries to imitate the bar chart
+3. Pictogram - I'm not so sure how I'll put this to you but it's sure to show the workout a lot better
+[This is to be removed when 0.1.0 is underway which is very close]
+press any integer key to exit
+
+"""
+
+HOW_Before = """
+
+Notes before you start the tutorial:
+1.BE case sensitive when typing into the input system.
+2.If you enter anything wrongly it will ask the question again.
+3.The password generator generates passwords at random so a password can't be the same for two computers
+
+Once you enter the DPJE app you'll see the sign in option as the first thing. For Example;
+
+--------------------------------------------------------------------------------------------------------
+Name: (your name)
+Age: (your age)
+Profession: (your profession)
+Birthday: (your birthdate)
+Do you have any other information: No
+Verify that: Your name is (your name), you were born on the (your birthdate), you are a (your profession) 
+and you are (your age) years old
+---------------------------------------------------------------------------------------------------------
+
+Take note of the Notes
+After you fill in the sign in options they'll ask you if you want to add some other notes
+it will then be left for you to make the decision whether you want to input it or not.
+if you pass this criteria, you'll enter into the password zone. For Example:
+
+---------------------------------------------------------------------------------------------------------
+Your password is : 7500007699
+Please enter your password: 7500007699
+---------------------------------------------------------------------------------------------------------
+
+Once you enter the password,you see something like this
+
+-------------------------------------------------------------------------------
+Welcome (your name) how can we help you
+1. Games
+2. Work
+Please input one of the integers listed above. Thank you as you do so.
+--------------------------------------------------------------------------------
+
+Enjoy the app.
+
+"""
+
+HOW = """
+
+Notes before you start the tutorial:
+1.BE case sensitive when typing into the input system.
+2.If you enter anything wrongly it will ask the question again.
+3.It now offers user defined password input do you can input any password of your choice
+
+Once you enter the DPJE app you'll see the sign in option as the first thing. For Example;
+
+--------------------------------------------------------------------------------------------------------
+Name: (your name)
+Age: (your age)
+Profession: (your profession)
+Birthday: (your birthdate)
+Do you have any other information: No
+
+NAME: (your name)
+BIRTHDAY: (your birthday)
+PROFESSION: (your profession)
+AGE: (your age)
+---------------------------------------------------------------------------------------------------------
+
+Take note of the Notes
+After you fill in the sign in options they'll ask you if you want to add some other notes
+it will then be left for you to make the decision whether you want to input it or not.
+if you pass this criteria, you'll enter into the password zone. For Example:
+
+---------------------------------------------------------------------------------------------------------
+Enter your password: sgt2873@_
+Confirm the password: sgt2873@_
+---------------------------------------------------------------------------------------------------------
+
+Once you enter the password,you see something like this
+
+-------------------------------------------------------------------------------
+Welcome (your name) how can we help you
+1. Games
+2. Work
+Please input one of the integers listed above. Thank you as you do so.
+--------------------------------------------------------------------------------
+
+Enjoy the app.
+
+"""
+
+MENU = """
+Help
+
+Welcome you must be a beginning user so what would you like to know:
+1. About Us and Updates
+2. How to do
+3. Credits
+press any key to exit
+
+"""
+
+CREDIT = """
+
+Front and Back end Programming: David George
+Editing and Writing: David George
+Design and Animation: Paul George
+Help: John the Beloved and Emmanuel George
+All these people listed above are the pioneers of DPJE Productions.
+
+"""
+
+GAMEMENU = """
+
+Games
+
+1. Tictactoe                      
+2. Chess                          
+3. EyeTest
+4. HangMan
+5. Rock-Paper-Scissors
+6. Guessing Game
+7. Tower Of Hanoi
+
+press any integer key to exit
+
+"""
+
+ABOUT = """
+
+Note:
+- Please check our HTML file named 'aboutUs.html' to know about us.
+- Please if there is any more information or update you want to be aware of go to the same file.
+- This decision will be changed in the upcoming versions.
+- Monopoly and some other games are still in-development.
+- The WorkBase [This is to be removed when 0.1.0 is underway which is very close] due to the uselessness.
+Version Code: 0.0.8
+
+Thank You.
+
+"""
+
+WELCOME = """
+
+1. Games
+2. Work
+press any integer key to exit
+
+"""
